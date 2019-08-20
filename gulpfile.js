a355beee9f0563880ff1c51015ce6131c394084c
qiqijojo <774948439@qq.com>
2019-08-20 18:55:00 +0800

    学习gulp创建task以及监听文件的配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,34 @@
-const { src, dest } = require('gulp');
-const babel = require('gulp-babel');
+const { src, dest, symlink, watch, task, series, parallel } = require('gulp');
+const chalk = require('chalk')
 
-exports.default = function() {
-  return src('*.js')
-    .pipe(babel())
-    .pipe(dest('output/'));
+function a(cb) {
+    cb()
 }
+function b(cb) {
+    cb()
+}
+function c(cb) {
+    cb()
+}
+function createDist(){ // 生成dist文件（src、dest/symlink使用）
+    return src('src/*.js')
+    .pipe(symlink('dist/'));
+}
+const tasks = [createDist, parallel([a, b, c])];
+function watching() {
+    console.log(chalk.red('正在监听文件...'))
+    const watcher = watch(['src/*.js']);
+    watcher.on('change', function(path, stats) {
+        console.log('更新：1', path, stats);
+    });
+    watcher.on('add', function(path, stats) {
+        console.log('更新：2', path, stats);
+    });
+}
+// 以上都是在定义task
+tasks.push(watching);
+function seriesTask() { // 将所有task顺序定为串行执行
+    return series(tasks)();
+}
+task('start', seriesTask); // 运行gulp执行的task
+
